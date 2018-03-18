0bb09a3dcdb8769bbe9bcb6aeec1ff85195896bf
bedanta1998 <bedanta1998@gmail.com>
2018-03-18 11:54:53 +0530

    version 0.0.1, ionic pro added, change to made in app.module.ts providers error handler.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { Pro } from '@ionic/pro';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, Injectable, Injector } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
@@ -20,6 +21,41 @@ import { PayPal } from '@ionic-native/paypal';
 
 import { IonicStorageModule } from '@ionic/storage';
 import { OneSignal } from "@ionic-native/onesignal";
+
+Pro.init('dc2ddcbe', {
+  appVersion: '0.0.1'
+})
+
+
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
+
+
+
+
+
+
+
 
 
 @NgModule({
@@ -57,6 +93,7 @@ import { OneSignal } from "@ionic-native/onesignal";
     SplashScreen,
     PayPal,
     OneSignal,
+    IonicErrorHandler,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
